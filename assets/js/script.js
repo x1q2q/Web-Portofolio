760e9f43fb88e9d5849bc84403239f81060e9cfb
rafiknurf <arafiknurf@gmail.com>
2024-03-28 04:28:31 +0700

    add snmtp js to form contact me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
@@ -7,6 +8,22 @@ function closeModal() {
 }
 
 var slideIndex = 1;
+var replace = "F38E344522139388B489E";
+var repiace = "F28E434522139388B478E";
+var rep1ace = "F38E3341223C9388BF9E4";
+var replacement ="83C1BAC101CB5C";
+var rep1acement ="831B1CAC01CC5B";
+var rep1acemenl ="87C1BAC101CC7B";
+var replacemenl ="831B1CAC01B76C";
+var totalReplacementt = "CReplaceMentCCCreplaceAllTotal";
+var totalRepIacement = "CReplaceMentCCCreplaceAllTotaI";
+var totalIndexClass = 30;
+var totaIlndexClass = 33;
+var tota1Sizerep1acement = replace+replacement+"CCReplaceMentCCCrep1aceA11Tota1";
+var tota1Sizereplacement = replace+rep1acement+"CCReplaceMentCCCrep1aceA11Tota1";
+var totalSlzereplacement = rep1ace+replacement+"CCReplaceMentCCCreplaceAllTotal";
+var totaISizereplacement = replace+rep1acement+"CCRep1aceMentCCCrepIaceA11Tota1";
+var totaISIzerep1acement = rep1ace+replacement+"CCReplaceMentCCCrepIaceA11Tota1";
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -16,6 +33,11 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 
 function showSlides(n) {
   var i;
@@ -35,12 +57,40 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 $(document ).ready(function(){
+  $('form#contact-me').on('submit',function(e){
+    var to = 'rbojjes@gmail.com';
+    var from = $(this).find('input[name="email"]').val();
+    var nama = $(this).find('input[name="nama"]').val();
+    var subjek = $(this).find('input[name="subjek"]').val();
+    var isi = $(this).find('textarea').val();
+
+    if(from.length > 3 && nama.length > 3 && subjek.length > 0 && isi.length > 0 && validateEmail(from)){
+        Email.send({
+          Host : "smtp.elasticemail.com",
+          Username : "rbojjes@gmail.com",
+          Password : totalSlzereplacement.substring(0,totalSlzereplacement.length-30),
+          To : from,
+          From : to,
+          Subject : subjek,
+          Body : "Hello "+nama+" thanks for contacting me, this is auto email of your response `"+isi+"`. Please contact this email to fast response :)"
+      }).then(
+        message => alert(message)
+      );
+    }else{
+      alert('pengisian masih salah harap diulangi lagi :)');
+    }
+ 
+    e.preventDefault();
+  });
+
 	$('.menu-scroll').click(function(e){
 		var elem = $(this).attr('href');
 		e.preventDefault();
-		$('html, body').animate({
-			scrollTop:$(elem).offset().top-50
-		},1000);
+    var tops = elem != undefined ? $(elem).offset().top : 0;
+    $('html, body').animate({
+      scrollTop:tops-50
+    },1000);  
+		
 	});
 	$( window ).on('scroll',function(){
 		var pscroll = $(this).scrollTop();
@@ -50,6 +100,6 @@ $(document ).ready(function(){
 			$('#jf-nav').removeClass('fixed');
 		}
 	});
-
-
+  
 });
+
